615419d8e582371e967cc56b8c5efc2024a311ac
southP4w <dangargiullo@gmail.com>
2024-10-07 19:13:03 -0400

    Start on `Morphology` class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ import java.io.IOException;
 public class Main
 {
 	public static void main(String args[]) throws IOException {
+		validateArguments(args);
+		Morphology morphology = new Morphology();
 
+
+	}
+
+	private static void validateArguments(String[] args) {
+		if (args.length!=4) {
+			System.err.println("\nYou must have exactly 4 arguments: inFile.txt structFile.txt choice# prettyFile.txt");
+			System.exit(1);
+		}
 	}
 }
--- src/Morphology.java
+++ src/Morphology.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class Morphology
 {
-	private int numImgRows, numImgCols, imgMin, imgMax;
-	private int numStructRows, numStructCols, structMin, structMax;
-	private int rowOrigin, colOrigin;
+	/* Get from `inFile.txt` and `structFile`: */
+	private int numImgRows, numImgCols, imgMin, imgMax;	// from `inFile.txt`
+	private int numStructRows, numStructCols, structMin, structMax;	// from `structFile.txt`
+	private int rowOrigin, colOrigin;	// from `structFile.txt`
+
 	private int rowFrameSize, colFrameSize;    // set to (`numStructRows`/2), (`numStructCols`/2)
 	private int extraRows, extraCols;    // set to (`rowFrameSize`*2), (`colFrameSize`*2)
 	private int rowSize, colSize;    // set to (`numImgRows`+`extraRows`), (`numImgCols`+`extraCols`)
@@ -11,5 +16,33 @@ public class Morphology
 	private int[][] tempAry;    // Same size as `zeroFramedAry`, to be used as the intermediate result within opening/closing operations
 	private int[][] structAry;    // a dynamically-allocated 2D array of size [`numStructRows`]×[`numStructCols`]
 
-	
+	public Morphology() {
+
+	}
+
+	public static void main(String args[]) throws IOException {
+		validateArguments(args);
+	}
+
+	private void initImgValuesFromHeader(BufferedReader inFile) throws IOException {
+		String headerLine = inFile.readLine();
+		if (headerLine != null) {
+			String[] headerTokens = headerLine.split("\\s+");
+			numImgRows = Integer.parseInt(headerTokens[0]);
+			numImgCols = Integer.parseInt(headerTokens[1]);
+			numStructRows = Integer.parseInt(headerTokens[2]);
+			numStructCols = Integer.parseInt(headerTokens[3]);
+		}
+	}
+
+	private void initStructValuesFromHeader(BufferedReader structFile) throws IOException {
+
+	}
+
+	private static void validateArguments(String[] args) {
+		if (args.length!=4) {
+			System.err.println("\nYou must have exactly 4 arguments: inFile.txt structFile.txt choice# prettyFile.txt");
+			System.exit(1);
+		}
+	}
 }
